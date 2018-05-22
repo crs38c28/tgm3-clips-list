bf6386880415d5a8f88d5552cf3aaec317f73ece
Crs <chris38c28@hotmail.com>
2018-05-23 02:08:06 +0800

    remove unused code

--- src/App.vue
+++ src/App.vue
@@ -78,13 +78,9 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  },
   data(){
     return {
       ClientID: ['kimne78kx3ncx6brgo4mv6wki5h1ko',
